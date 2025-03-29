d68254b26035c0beb4f7463686a48e6b3fe9c693
johneliud <johneliud4@gmail.com>
2025-03-29 13:28:35 +0300

    add: define User model with fields for user data and verification status

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,1 +1,11 @@
 package models
+
+type User struct {
+	ID                int    `json:"id"`
+	FirstName         string `json:"first_name"`
+	LastName          string `json:"last_name"`
+	Email             string `json:"email"`
+	Password          string `json:"password"`
+	ConfirmedPassword string `json:"confirmed_password"`
+	Verified          bool   `json:"verified"`
+}
