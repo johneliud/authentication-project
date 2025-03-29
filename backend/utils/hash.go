4a079e81ce75a131aaa04c5448fe9f98af25443e
johneliud <johneliud4@gmail.com>
2025-03-29 13:33:36 +0300

    feat: implement HashPassword function for secure password hashing using bcrypt

--- backend/utils/hash.go
+++ backend/utils/hash.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword uses bcrypt to hash a password with a cost of 12 for secure storage in the database.
+func HashPassword(password []byte, cost int) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, cost)
+	if err != nil {
+		return "", fmt.Errorf("could not hash password: %w", err)
+	}
+
+	return string(hashedPassword), nil
+}
