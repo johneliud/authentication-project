4da9a1b33fbae03dd00ae6daa69fc7daa1a3dd7e
johneliud <johneliud4@gmail.com>
2025-03-29 16:11:23 +0300

    add: client side sign up validation and provide feedback on every action or event.

---
+++ frontend/js/signup.js
@@ -0,0 +1,80 @@
+import { showFeedback } from './script.js';
+
+// validatePassword checks if the password is valid
+function validatePassword(password) {
+  if (password.length < 8) return 'Password must be at least 8 characters long';
+
+  if (!password.match(/[A-Z]/))
+    return 'Password must contain at least one uppercase letter';
+
+  if (!password.match(/[a-z]/))
+    return 'Password must contain at least one lowercase letter';
+
+  if (!password.match(/[0-9]/))
+    return 'Password must contain at least one number';
+
+  if (!password.match(/[^A-Za-z0-9]/))
+    return 'Password must contain at least one special character';
+
+  return '';
+}
+
+const passwordInput = document.getElementById('password');
+const confirmedPasswordInput = document.getElementById('confirmedPassword');
+
+passwordInput.addEventListener('input', () => {
+  const password = passwordInput.value;
+  const confirmedPassword = confirmedPasswordInput.value;
+  const passwordError = validatePassword(password);
+  const confirmedPasswordError = validateConfirmedPassword(
+    password,
+    confirmedPassword
+  );
+  showFeedback(passwordError, false);
+  showFeedback(confirmedPasswordError, false);
+});
+
+// validateConfirmedPassword checks if the confirmed password is valid
+function validateConfirmedPassword(password, confirmedPassword) {
+  if (password !== confirmedPassword) return 'Passwords do not match';
+  return '';
+}
+
+const signupForm = document.getElementById('signupForm');
+
+signupForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const submitBtn = document.getElementById('signupBtn');
+  submitBtn.textContent = 'Signing Up...';
+  submitBtn.disabled = true;
+
+  const formData = new FormData(signupForm);
+
+  try {
+    const response = await fetch('/sign-up', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      showFeedback(data.message, data.success);
+    } else {
+      const error = await response.json();
+      showFeedback(error.message, false);
+    }
+    signupForm.reset();
+    submitBtn.textContent = 'Sign Up';
+    submitBtn.disabled = false;
+
+    setTimeout(() => {
+      window.location.href = '/verify';
+    }, 1000);
+  } catch (error) {
+    console.error(error);
+  }
+});
