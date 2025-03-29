d175443198a8541aca03694a9e5e3881d34a00a4
johneliud <johneliud4@gmail.com>
2025-03-29 13:47:09 +0300

    feat: add InitDB function to initialize the database and execute the schema

--- backend/database/db.go
+++ backend/database/db.go
@@ -3,8 +3,32 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 )
+
+// InitDB initializes the database and returns a pointer to the database.
+func InitDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./data/authentication.db")
+	if err != nil {
+		log.Printf("Could not open database: %v\n", err)
+		return nil
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Printf("Could not ping database: %v\n", err)
+		return nil
+	}
+
+	err = executeSchema(db)
+	if err != nil {
+		log.Printf("Could not execute schema: %v\n", err)
+		return nil
+	}
+	log.Println("Database initialized successfully")
+	return db
+}
 
 // executeSchema reads the schema file and executes it on the database.
 func executeSchema(db *sql.DB) error {
