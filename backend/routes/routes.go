02264710e1a81a249421789cfb85fec387554e52
johneliud <johneliud4@gmail.com>
2025-03-29 14:17:41 +0300

    feat: initialize server routes for frontend and authentication handlers

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/johneliud/authentication_project/backend/handlers"
+)
+
+// InitRoutes initializes the routes for the server.
+func InitRoutes() {
+	fs := http.FileServer(http.Dir("frontend"))
+	http.Handle("/frontend/", http.StripPrefix("/frontend", fs))
+
+	http.HandleFunc("/sign-up", handlers.SignupHandler)
+	http.HandleFunc("/verify", handlers.VerifyHandler)
+	http.HandleFunc("/sign-in", handlers.SigninHandler)
+}
