c6010355a287097d34e9d2d3c88f333f3d139bde
johneliud <johneliud4@gmail.com>
2025-03-29 19:57:28 +0300

    update: ValidateUserFields() function to use trim white spaces in a field before checking if its empty.

--- backend/utils/validate_user_fields.go
+++ backend/utils/validate_user_fields.go
@@ -1,30 +1,33 @@
 package utils
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 // ValidateUserFields validates the user fields.
 func ValidateUserFields(firstName, lastName, email, password, confirmedPassword string) error {
-	if firstName == "" {
+	if strings.TrimSpace(firstName) == "" {
 		return errors.New("first name cannot be empty")
 	}
 
-	if lastName == "" {
+	if strings.TrimSpace(lastName) == "" {
 		return errors.New("last name cannot be empty")
 	}
 
-	if email == "" {
+	if strings.TrimSpace(email) == "" {
 		return errors.New("email cannot be empty")
 	}
 
-	if password == "" {
+	if strings.TrimSpace(password) == "" {
 		return errors.New("password cannot be empty")
 	}
 
-	if confirmedPassword == "" {
+	if strings.TrimSpace(confirmedPassword) == "" {
 		return errors.New("confirmed password cannot be empty")
 	}
 
-	if password != confirmedPassword {
+	if strings.TrimSpace(password) != strings.TrimSpace(confirmedPassword) {
 		return errors.New("passwords do not match")
 	}
 	return nil
