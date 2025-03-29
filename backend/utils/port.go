36d2aa484d010de26139fb54e822ecc217303f5a
johneliud <johneliud4@gmail.com>
2025-03-29 14:03:02 +0300

    feat: add GetPort function to retrieve and validate port number from command line arguments

---
+++ backend/utils/port.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+// GetPort returns the port number from the command line arguments and returns the default port 10000 in not provided.
+func GetPort() (string, error) {
+	var (
+		err  error
+		port string
+	)
+
+	switch len(os.Args) {
+	case 2:
+		port = os.Args[1]
+
+		portNum, err := strconv.Atoi(port)
+		if err != nil || portNum < 1024 || portNum > 65535 {
+			log.Printf("Failed validating port: %v\n\nSwitching to default port 10000\n", err)
+			port = ":10000"
+		} else {
+			port = ":" + port
+		}
+	default:
+		port = ":10000"
+	}
+	return port, err
+}
