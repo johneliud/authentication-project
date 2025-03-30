1d8580298b7bdcc497a30cd29bdec964b2524e94
johneliud <johneliud4@gmail.com>
2025-03-30 23:46:11 +0300

    update: InitSession function to correctly retrieve session key stringn from .env file.

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,15 +1,29 @@
 package config
 
 import (
+	"log"
 	"os"
 
 	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 )
 
-var Store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+var Store *sessions.CookieStore
 
 // InitSession configures the session store.
 func InitSession() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading .env file: %v\n", err)
+	}
+
+	sessionKey := os.Getenv("SESSION_KEY")
+	if sessionKey == "" {
+		log.Println("Warning: SESSION_KEY is not set or empty")
+		return
+	}
+
+	Store = sessions.NewCookieStore([]byte(sessionKey))
 	Store.Options = &sessions.Options{
 		Path:     "/",
 		MaxAge:   3600 * 24, // 24 hours
