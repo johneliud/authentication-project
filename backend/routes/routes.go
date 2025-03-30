961ba1a7f97697690f3ea5e26e3b9722918f58e5
johneliud <johneliud4@gmail.com>
2025-03-31 00:37:51 +0300

    feat: implement session middleware for authentication routes and add logout handler

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/johneliud/authentication_project/backend/handlers"
+	"github.com/johneliud/authentication_project/backend/middleware"
 )
 
 // InitRoutes initializes the routes for the server.
@@ -11,8 +12,23 @@ func InitRoutes() {
 	fs := http.FileServer(http.Dir("frontend"))
 	http.Handle("/frontend/", http.StripPrefix("/frontend", fs))
 
-	http.HandleFunc("/sign-up", handlers.SignupHandler)
-	http.HandleFunc("/verify", handlers.VerifyHandler)
-	http.HandleFunc("/sign-in", handlers.SigninHandler)
-	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/sign-up", func(w http.ResponseWriter, r *http.Request) {
+		middleware.SessionMiddleware(http.HandlerFunc(handlers.SignupHandler)).ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/verify", func(w http.ResponseWriter, r *http.Request) {
+		middleware.SessionMiddleware(http.HandlerFunc(handlers.VerifyHandler)).ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/sign-in", func(w http.ResponseWriter, r *http.Request) {
+		middleware.SessionMiddleware(http.HandlerFunc(handlers.SigninHandler)).ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		middleware.SessionMiddleware(http.HandlerFunc(handlers.HomeHandler)).ServeHTTP(w, r)
+	})
+	
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		middleware.SessionMiddleware(http.HandlerFunc(handlers.LogoutHandler)).ServeHTTP(w, r)
+	})
 }
