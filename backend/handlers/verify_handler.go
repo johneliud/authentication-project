3c91c7c49c92c65d3419964e045860d46b1112ee
johneliud <johneliud4@gmail.com>
2025-03-30 12:05:15 +0300

    update: handler to retrieve the verification stored in the database and compare with the provided code to determine correctness.

--- backend/handlers/verify_handler.go
+++ backend/handlers/verify_handler.go
@@ -1,9 +1,14 @@
 package handlers
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/johneliud/authentication_project/backend/database"
+	"github.com/johneliud/authentication_project/backend/models"
 )
 
 // VerifyHandler handles the verification page.
@@ -27,6 +32,57 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 			log.Printf("Failed to execute template: %v\n", err)
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		}
+	case http.MethodPost:
+		var user models.User
+
+		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+			log.Printf("Failed to decode request body: %v\n", err)
+			w.Header().Set("Content-Type", "application/json")
+			response := Response{Success: false, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+		defer r.Body.Close()
+
+		var dbCode string
+
+		err := database.DB.QueryRow("SELECT verification_code FROM users WHERE email = ?", user.Email).Scan(&dbCode)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				log.Printf("Email %q found: %v\n", user.Email, err)
+				w.Header().Set("Content-Type", "application/json")
+				response := Response{Success: false, Message: "Email not found"}
+				json.NewEncoder(w).Encode(response)
+				return
+			} else {
+				log.Printf("Failed to query database: %v\n", err)
+				w.Header().Set("Content-Type", "application/json")
+				response := Response{Success: false, Message: err.Error()}
+				json.NewEncoder(w).Encode(response)
+				return
+			}
+		}
+
+		if dbCode != user.VerificationCode {
+			log.Printf("Verification code %q does not match: %v\n", user.VerificationCode, err)
+			w.Header().Set("Content-Type", "application/json")
+			response := Response{Success: false, Message: "Verification code does not match"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		_, err = database.DB.Exec("UPDATE users SET verified = 1 WHERE email = ?", user.Email)
+		if err != nil {
+			log.Printf("Failed to update user: %v\n", err)
+			w.Header().Set("Content-Type", "application/json")
+			response := Response{Success: false, Message: err.Error()}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		response := Response{Success: true, Message: "User verified"}
+		json.NewEncoder(w).Encode(response)
 
 	default:
 		log.Printf("Method not allowed: %s\n", r.Method)
