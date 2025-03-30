e9299c66f32338363285cdc478f5ef82f01c846f
johneliud <johneliud4@gmail.com>
2025-03-30 15:00:11 +0300

    add: InitSession() function to configure the session store.

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"os"
+
+	"github.com/gorilla/sessions"
+)
+
+var Store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+// InitSession configures the session store.
+func InitSession() {
+	Store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600 * 24, // 24 hours
+		HttpOnly: true,
+	}
+}
