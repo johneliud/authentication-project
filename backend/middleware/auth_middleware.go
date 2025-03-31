5e24c2bf8d3980a3c1e8012095e8f596dc446856
johneliud <johneliud4@gmail.com>
2025-03-31 10:44:44 +0300

    update: modify session middleware to exempt verification route and redirect to sign-up

--- backend/middleware/auth_middleware.go
+++ backend/middleware/auth_middleware.go
@@ -9,20 +9,19 @@ import (
 // SessionMiddleware is a middleware that checks if the user is authenticated and redirects to the sign-in page if not.
 func SessionMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Exempt signup and signin routes from requiring a session
-		if r.URL.Path == "/sign-up" || r.URL.Path == "/sign-in" {
+		if r.URL.Path == "/sign-up" || r.URL.Path == "/verify" || r.URL.Path == "/sign-in" {
 			next.ServeHTTP(w, r)
 			return
 		}
 
 		session, err := config.Store.Get(r, "session")
 		if err != nil {
-			http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+			http.Redirect(w, r, "/sign-up", http.StatusSeeOther)
 			return
 		}
 
 		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-			http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+			http.Redirect(w, r, "/sign-up", http.StatusSeeOther)
 			return
 		}
 
